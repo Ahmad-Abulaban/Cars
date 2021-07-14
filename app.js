56420a919363321f743485ea562a95cade51c81b
AhmedAbulaban <>
2021-07-14 13:19:52 +0000

    Update js

---
+++ app.js
@@ -0,0 +1,30 @@
+var name = prompt("what is your name")
+
+alert("Hey, Wlecome to my Website " + name )
+
+var favPhone = prompt("what is you fav Phone?")
+
+
+if(favPhone == "iphone"){
+document.write(
+  "<div>" + "<h3>" + favPhone+"</h3>"
++ "<img  src='iphone.jfif' width='100' height='100'     />" + "</div>")
+}
+else if (favPhone =="samsung"){
+document.write(
+  "<div>" + "<h3>" + favPhone+"</h3>"
++ "<img  src='samsung.jfif' width='100' height='100'     />" + "</div>")
+}
+else if(favPhone =="huawei"){
+document.write(
+  "<div>" + "<h3>" + favPhone+"</h3>"
++ "<img  src='huawei.jfif' width='100' height='100'     />" + "</div>")
+}
+else if(favPhone =="Redmi"){
+document.write(
+  "<div>" + "<h3>" + favPhone+"</h3>"
++ "<img  src='Redmi.jfif' width='100' height='100'     />" + "</div>")
+}
+else{
+  alert('we donot have this Phone')
+}
