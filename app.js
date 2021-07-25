045945cb62c707be64fe895c7f34dcd60ca19bea
AhmedAbulaban <>
2021-07-25 11:42:57 +0000

    loops

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var name = prompt("what is your name")
 
-alert("Hey, Wlecome to my Website " + name )
+alert("Hey, Wlecome to my Website " + name)
 
-var favPhone = prompt("what is you fav Phone?")
+var favPhone = prompt("what is you fav Phone? , please choose one of these ; iphone , samsung , huawei , Redmi ")
+
+while (favPhone != "iphone" && favPhone != "samsung" && favPhone != "huawei" && favPhone != "Redmi") {
+
+  favPhone = prompt("what is you fav Phone? , please choose one of these ; iphone , samsung , huawei , Redmi ")
+
+}
+
+var numberOfPic = prompt("how many times you wish to see it ")
+
+while (numberOfPic > 10) {
+  numberOfPic = prompt("please enter number less than 10!")
+}
 
 
 var txt
@@ -14,26 +26,31 @@ if (r == true) {
 }
 
 
-if(favPhone == "iphone"){
-document.write(
-  "<div>" + "<h3>" + favPhone+"</h3>"
-+ "<img  src='Images/iphone.jfif' width='100' height='100'     />" + "</div>")
+if (favPhone == "iphone") {
+
+  for (var i = 0; i < numberOfPic; i++)
+    document.write(
+      "<div>" + "<h3>" + favPhone + "</h3>"
+      + "<img  src='Images/iphone.jfif' width='150' height='150'     />" + "</div>")
 }
-else if (favPhone == "samsung"){
-document.write(
-  "<div>" + "<h3>" + favPhone+"</h3>"
-+ "<img  src='Images/samsung.jfif' width='100' height='100'     />" + "</div>")
+else if (favPhone == "samsung") {
+  for (var i = 0; i < numberOfPic; i++)
+    document.write(
+      "<div>" + "<h3>" + favPhone + "</h3>"
+      + "<img  src='Images/samsung.jfif' width='150' height='150'     />" + "</div>")
 }
-else if(favPhone == "huawei"){
-document.write(
-  "<div>" + "<h3>" + favPhone+"</h3>"
-+ "<img  src='Images/huawei.jfif' width='100' height='100'     />" + "</div>")
+else if (favPhone == "huawei") {
+  for (var i = 0; i < numberOfPic; i++)
+    document.write(
+      "<div>" + "<h3>" + favPhone + "</h3>"
+      + "<img  src='Images/huawei.jfif' width='150' height='150'     />" + "</div>")
 }
-else if(favPhone == "Redmi"){
-document.write(
-  "<div>" + "<h3>" + favPhone+"</h3>"
-+ "<img  src='Images/Redmi.jfif' width='100' height='100'     />" + "</div>")
+else if (favPhone == "Redmi") {
+  for (var i = 0; i < numberOfPic; i++)
+    document.write(
+      "<div>" + "<h3>" + favPhone + "</h3>"
+      + "<img  src='Images/Redmi.jfif' width='150' height='150'     />" + "</div>")
 }
-else{
+else {
   alert('we donot have this Phone')
 }
