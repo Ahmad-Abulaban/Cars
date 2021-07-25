3b9cd0a549300ec8679ad12ec6dcc985d787f849
AhmedAbulaban <>
2021-07-25 13:47:37 +0000

    Loops v2

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+function intersted_in_phones(){
+var p = prompt("Do you like phones? , please type yes or no . You should type yes to contion")
+while(p != "yes"){
+  p = prompt("Do you like phones? , please type yes or no . You should type yes to contion")
+}
+}
+
 var name = prompt("what is your name")
 
 alert("Hey, Wlecome to my Website " + name)
+
+intersted_in_phones()
 
 var favPhone = prompt("what is you fav Phone? , please choose one of these ; iphone , samsung , huawei , Redmi ")
 
@@ -17,21 +26,13 @@ while (numberOfPic > 15) {
 }
 
 
-var txt
-var r = confirm("Do you like phones?")
-if (r == true) {
-  txt = "You pressed Yes!"
-} else {
-  txt = "You pressed No!"
-}
 
 
 if (favPhone == "iphone") {
-
   for (var i = 0; i < numberOfPic; i++)
     document.write(
       "<div>" + "<h3>" + favPhone + "</h3>"
-      + "<img  src='Images/iphone.jfif' width='150' height='150'     />" + "</div>")
+      + "<img  src='Images/iphone.jfif' width='150' height='150'     />" + "</div>") 
 }
 else if (favPhone == "samsung") {
   for (var i = 0; i < numberOfPic; i++)
